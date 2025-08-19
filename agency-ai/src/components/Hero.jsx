ce63071ac15b316750589dd5ccb46f5934a51bad
Baiju Thomas <baijubaby4880@gmail.com>
2025-08-19 23:31:08 +0530

    updated the h1 tag

--- agency-ai/src/components/Hero.jsx
+++ agency-ai/src/components/Hero.jsx
@@ -9,6 +9,9 @@ const Hero = () => {
             <img className='w-20' src={assets.group_profile} alt="" />
             <p className='text-xs font-medium'>Trusted by 10k+ people</p>
         </div>
+
+        <h1 className='text-4xl sm:text-5xl md:text-6xl xl:text-[84px] font-medium xl:leading-[95px] max-w-5xl'>Turning imagination into 
+          <span className='bg-gradient-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent'>digital</span> impact.</h1>
       
     </div>
   )
