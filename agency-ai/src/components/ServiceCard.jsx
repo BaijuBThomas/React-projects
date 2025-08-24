3efc575301de44d5a2d6528fe2a57d544a16a765
Baiju Thomas <baijubaby4880@gmail.com>
2025-08-24 15:44:40 +0530

    Adding new updateds

--- agency-ai/src/components/ServiceCard.jsx
+++ agency-ai/src/components/ServiceCard.jsx
@@ -4,11 +4,12 @@ import assets from '../assets/assets'
 const ServiceCard = ({service, index}) => {
 
     const [position, setposition] = useState({x:0, y:0 })
+    const [visible, setvisible] = useState(false);
 
   return (
-    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border dark:shadow-white/10'>
+    <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border dark:shadow-white/10' onMouseEnter={()=>setvisible(true)} onMouseLeave={()=>setvisible(false)}>
 
-        <div className='pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten opacity-70' style={{top:position.y - 150, left: position.x - 150}} />
+        <div className={`pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten ${visible ? 'opacity-70' : 'opacity-0'}`} style={{top:position.y - 150, left: position.x - 150}} />
 
             <div className='flex item-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded-[10px] bg-white dark:bg-gray-900 z-10 relative'>
 
