52af91368d7c51a4e03ce3cdf89f111ed0bba2b7
Baiju Thomas <baijubaby4880@gmail.com>
2025-08-28 21:43:14 +0530

    The comple project

--- agency-ai/src/components/ContactUs.jsx
+++ agency-ai/src/components/ContactUs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Title from './Title'
 import assets from '../assets/assets'
 import toast from 'react-hot-toast';
+import { motion } from "motion/react"
 
 const ContactUs = () => {
     const onSubmit  = async (event) => {
@@ -33,10 +34,21 @@ const ContactUs = () => {
     };
 
   return (
-    <div id='contact-us' className='flex flex-col items-center gap-7 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white' >
+    <motion.div 
+    initial= "hidden"
+    whileInView="visible"
+    transition={{staggerChildren: 0.2}}
+    viewport={{once: true}}
+    
+    id='contact-us' className='flex flex-col items-center gap-7 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white' >
       <Title title='Reach out to us' desc='From strategy to execution, we craft digital solutions that move your business forward.' />
 
-      <form  onSubmit={onSubmit } className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
+      <motion.form 
+      initial={{opacity:0, y: 30}}
+      whileInView={{opacity:1, y:0}}
+      transition={{duration: 0.6, delay: 0.4}}
+      viewport={{once: true}}
+      onSubmit={onSubmit } className='grid sm:grid-cols-2 gap-3 sm:gap-5 max-w-2xl w-full'>
 
         <div>
             <p className='mb-2 text-sm font-medium'>Your name</p>
@@ -62,8 +74,8 @@ const ContactUs = () => {
         <button type='submit' className='w-max flex gap-2 bg-primary text-white text-sm px-10 py-3 rounded-full cursor-pointer hover:scale-103 transition-all' >
             submit <img src={assets.arrow_icon} alt="" className='w-4' />
         </button>
-      </form>
-    </div>
+      </motion.form>
+    </motion.div>
   )
 }
 
