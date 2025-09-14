b5a899fc7df20bb09c5c1cc8b2526d7fc9747617
Baiju Thomas <baijubaby4880@gmail.com>
2025-09-14 22:01:58 +0530

    Adding new files

--- quickshow/client/src/App.jsx
+++ quickshow/client/src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Routes } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favourite from './pages/Favourite'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home/>}
+        <Route path='/' element={<Home/>} />
+        <Route path='/movies' element={<Movies/>} />
+        <Route path='/movies/:id' element={<MovieDetails/>} />
+        <Route path='/movies/:id/:date' element={<SeatLayout/>} />
+        <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path='/Favourite' element={<Favourite/>} />
       </Routes>
     </>
   )
